97425edf9ea5047343ebc14706ca4186046325a3
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-08-26 14:43:57 -0700

    add horizontal_victory checker

--- lib/victory_checker.rb
+++ lib/victory_checker.rb
@@ -1,3 +1,11 @@
 class VictoryChecker
+  THREE_CONSECUTIVE_Xs = ['X','X','X']
+  THREE_CONSECUTIVE_Os = ['O','O','O']
+  def horizontal_win(grid)
+    top = grid.values_at(1, 2, 3)
+    mid = grid.values_at(4, 5, 6)
+    bottom = grid.values_at(7, 8, 9)
 
+    'X' if [top, mid, bottom].include?(THREE_CONSECUTIVE_Xs)
+  end
 end
--- spec/lib/victory_checker_spec.rb
+++ spec/lib/victory_checker_spec.rb
@@ -3,6 +3,12 @@ require 'victory_checker'
 
 describe VictoryChecker do
   describe '#horizontal_win' do
-    
+    it "should return 'X' if any row has all X's" do
+      victory_checker = VictoryChecker.new
+      board = double('board', 'grid' => {1 => ' ', 2 => ' ', 3 => ' ',
+                                       4 => ' ', 5 => ' ', 6 => ' ',
+                                       7 => 'X', 8 => 'X', 9 => 'X'})
+      expect(victory_checker.horizontal_win(board.grid)).to eq 'X'
+    end
   end
 end
