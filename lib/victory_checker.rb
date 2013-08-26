0523b189650c0049fab248d05b9249c969f007fb
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-08-26 15:30:02 -0700

    refactor victory method to use one method to check for win instead of three

--- lib/victory_checker.rb
+++ lib/victory_checker.rb
@@ -2,29 +2,22 @@ class VictoryChecker
   THREE_CONSECUTIVE_Xs = ['X','X','X']
   THREE_CONSECUTIVE_Os = ['O','O','O']
 
-  def horizontal_win(grid)
+  def check_for_win(grid)
+    combinations = winning_combinations(grid)
+    return 'X' if combinations.include?(THREE_CONSECUTIVE_Xs)
+    return 'O' if combinations.include?(THREE_CONSECUTIVE_Os)
+  end
+
+  def winning_combinations(grid)
     top = grid.values_at(1, 2, 3)
-    mid = grid.values_at(4, 5, 6)
+    middle_row = grid.values_at(4, 5, 6)
     bottom = grid.values_at(7, 8, 9)
-
-    return 'X' if [top, mid, bottom].include?(THREE_CONSECUTIVE_Xs)
-    return 'O' if [top, mid, bottom].include?(THREE_CONSECUTIVE_Os)
-  end
-
-  def vertical_win(grid)
     left = grid.values_at(1, 4, 7)
-    mid = grid.values_at(2, 5, 8)
+    middle_column = grid.values_at(2, 5, 8)
     right = grid.values_at(3, 6, 9)
-
-    return 'X' if [left, mid, right].include?(THREE_CONSECUTIVE_Xs)
-    return 'O' if [left, mid, right].include?(THREE_CONSECUTIVE_Os)
-  end
-
-  def diagonal_win(grid)
     left_across = grid.values_at(1, 5, 9)
     right_across = grid.values_at(3, 5, 7)
 
-    return 'X' if [left_across, right_across].include?(THREE_CONSECUTIVE_Xs)
-    return 'O' if [left_across, right_across].include?(THREE_CONSECUTIVE_Os)
+    [top, middle_row, bottom, left, middle_column, right, left_across, right_across]
   end
 end
--- spec/lib/victory_checker_spec.rb
+++ spec/lib/victory_checker_spec.rb
@@ -6,54 +6,50 @@ describe VictoryChecker do
                                                   4 => ' ', 5 => ' ', 6 => ' ',
                                                   7 => ' ', 8 => ' ', 9 => ' '})}
   let(:victory_checker) { VictoryChecker.new }
-  context '#horizontal_win' do
+  describe '#check_for_win' do
     it "should return 'X' if any row has all X's" do
       board.grid[7], board.grid[8], board.grid[9] = 'X', 'X', 'X'
-      expect(victory_checker.horizontal_win(board.grid)).to eq 'X'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'X'
     end
 
     it "should return 'O' if any row has all O's" do
       board.grid[4], board.grid[5], board.grid[6] = 'O', 'O', 'O'
-      expect(victory_checker.horizontal_win(board.grid)).to eq 'O'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'O'
     end
 
     it "should return nil if there are no horizontal wins" do
       board.grid[4], board.grid[5] = 'O', 'O'
-      expect(victory_checker.horizontal_win(board.grid)).to be_nil
+      expect(victory_checker.check_for_win(board.grid)).to be_nil
     end
-  end
 
-  context "vertical_win" do
     it "should return 'X' if any column has all X's" do
       board.grid[2], board.grid[5], board.grid[8] = 'X', 'X', 'X'
-      expect(victory_checker.vertical_win(board.grid)).to eq 'X'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'X'
     end
 
     it "should return 'O' if any column has all O's" do
       board.grid[1], board.grid[4], board.grid[7] = 'O', 'O', 'O'
-      expect(victory_checker.vertical_win(board.grid)).to eq 'O'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'O'
     end
 
     it "should return nil if there are no vertical wins" do
       board.grid[1], board.grid[7] = 'O', 'O'
-      expect(victory_checker.vertical_win(board.grid)).to be_nil
+      expect(victory_checker.check_for_win(board.grid)).to be_nil
     end
-  end
 
-  context "diagonal_win" do
     it "should return 'X' if any column has all X's" do
       board.grid[1], board.grid[5], board.grid[9] = 'X', 'X', 'X'
-      expect(victory_checker.diagonal_win(board.grid)).to eq 'X'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'X'
     end
 
     it "should return 'O' if any column has all O's" do
       board.grid[3], board.grid[5], board.grid[7] = 'O', 'O', 'O'
-      expect(victory_checker.diagonal_win(board.grid)).to eq 'O'
+      expect(victory_checker.check_for_win(board.grid)).to eq 'O'
     end
 
     it "should return nil if there are no vertical wins" do
       board.grid[1], board.grid[9] = 'O', 'O'
-      expect(victory_checker.diagonal_win(board.grid)).to be_nil
+      expect(victory_checker.check_for_win(board.grid)).to be_nil
     end
   end
 end
