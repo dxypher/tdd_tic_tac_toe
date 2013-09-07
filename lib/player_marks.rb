b80c938db0cd59bad57e6251df81daa3b3173f36
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-09-07 13:06:47 -0700

    add a test for board#set_board_marks

--- lib/player_marks.rb
+++ lib/player_marks.rb
@@ -1,2 +1,6 @@
+first_player = 'X'
+second_player = 'O'
+
+
 COMPUTER_MARK = 'X'
 HUMAN_MARK = 'O'
--- spec/lib/ui_spec.rb
+++ spec/lib/ui_spec.rb
@@ -1,16 +1,33 @@
 require 'spec_helper'
 
 describe UI do
+  before(:each) do
+    # COMPUTER_MARK = 'X'
+    # HUMAN_MARK = 'O'
+    @board = double('board', 'grid' => {1 => ' ', 2 => ' ', 3 => 'human',
+                                       4 => ' ', 5 => ' ', 6 => ' ',
+                                       7 => 'computer', 8 => ' ', 9 => ' '})
+  end
   describe '#print_board' do
     it "should print the current board to the console" do
       ui = UI.new
-      COMPUTER_MARK = 'X'
-      board = double('board', 'grid' => {1 => ' ', 2 => ' ', 3 => ' ',
-                                         4 => ' ', 5 => ' ', 6 => ' ',
-                                         7 => 'computer', 8 => ' ', 9 => ' '})
-      expected = "      |     |   \n  _______________\n      |     |   \n  _______________\n   X  |     |   \n\n"
-      output = capture_stdout { ui.print_board(board.grid) }
+      expected = "      |     |  #{HUMAN_MARK}\n  _______________\n      |     |   \n  _______________\n   #{COMPUTER_MARK}  |     |   \n\n"
+      output = capture_stdout { ui.print_board(@board.grid) }
       expect(output).to eq expected
+    end
+  end
+
+  describe '#set_board_marks' do
+    it "should set a spot owned by the computer with the COMPUTER_MARK" do
+      ui = UI.new
+      new_grid = ui.set_board_marks(@board.grid)
+      expect(new_grid[7]).to eq COMPUTER_MARK
+    end
+
+    it "should set a spot owned by the human player with the HUMAN_MARK" do
+      ui = UI.new
+      new_grid = ui.set_board_marks(@board.grid)
+      expect(new_grid[3]).to eq HUMAN_MARK
     end
   end
 end
